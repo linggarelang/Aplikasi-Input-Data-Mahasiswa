b2087715fa169002192e833dd55b087fffed94b5
linggarelang <linggar.elangp@students.amikom.ac.id>
2023-09-27 23:54:21 +0700

    Add project files.

---
+++ AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace AplikasiInputDataMahasiswa
+{
+    internal class Mahasiswa
+    {
+        internal object nilaiHuruf;
+
+        public string Nama { get; internal set; }
+        public string Nim { get; internal set; }
+        public string Kelas { get; internal set; }
+        public int Nilai { get; internal set; }
+        public string Huruf;
+    }
+}
